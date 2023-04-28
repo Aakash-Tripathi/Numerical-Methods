b262b3688a2c9d30895651bdde83f0b8ceaf653e
Aakash Tripathi <aakash.tripathi0304@gmail.com>
2023-04-27 22:07:30 -0400

    rk fixed

--- project/methods.py
+++ project/methods.py
@@ -22,8 +22,5 @@ def rk_integration(func, y0, t, beta, gamma, alpha, n):
         k2 = func(y[i - 1] + dt / 2 * k1, t[i - 1] + dt / 2, beta, gamma, alpha, n)
         k3 = func(y[i - 1] + dt / 2 * k2, t[i - 1] + dt / 2, beta, gamma, alpha, n)
         k4 = func(y[i - 1] + dt * k3, t[i - 1] + dt, beta, gamma, alpha, n)
-        # if alpha is None:
-        #     y[i] = y[i - 1] + dt * (k1 + k2 + k3) / 3
-        # else:
         y[i] = y[i - 1] + dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
     return y
